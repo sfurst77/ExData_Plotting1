d592d2553fa9e9328244bdefdf33b7a73487e289
sfurst77 <32624225+sfurst77@users.noreply.github.com>
2018-01-27 15:25:34 -0800

    Add files via upload

--- plot1.R
+++ plot1.R
@@ -3,6 +3,9 @@
     
 ## read in data
     powerdata <- read.table("C:/Users/Steve/Desktop/Coursera/household_power_consumption.txt", sep=";", header = TRUE, stringsAsFactors = FALSE)
+
+## select dates
+    powerdata <- subset(powerdata, Date == "1/2/2007" | Date == "2/2/2007")
     
 ## Create first plot (histogram)
     hist(as.numeric(powerdata$Global_active_power), xlim = range(0:6), col = "red", xlab = "Global Active Power (kilowatts)", main = "Global Active Power")
